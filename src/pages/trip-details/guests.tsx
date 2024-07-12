22bd8483ff5abe946436a366e5fb8d2f43af3713
Hudson Holanda <hudsonholanda55@gmail.com>
2024-07-12 20:00:31 -0300

    feat: create guests component

---
+++ src/pages/trip-details/guests.tsx
@@ -0,0 +1,34 @@
+import { CircleDashed, UserCog } from "lucide-react";
+import { Button } from "../../components/button";
+
+export function Guests() {
+  return (
+    <div className="space-y-6">
+      <h2 className="font-semibold text-xl">Convidados</h2>
+      <div className="space-y-5">
+        <div className="flex items-center justify-between">
+          <div className="space-y-5">
+            <span className="block font-medium  text-zinc-100">Jessia White</span>
+            <span className="block text-xs truncate text-zinc-400 hover:text-zinc-200">
+              jessicawhite44@yahoo.com.br
+            </span>
+          </div>
+          <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="space-y-5">
+            <span className="block font-medium  text-zinc-100">Dr. Rita Pacocha</span>
+            <span className="block text-xs truncate text-zinc-400 hover:text-zinc-200">
+              ritapacocha@yahoo.com.br
+            </span>
+          </div>
+          <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+        </div>
+      </div>
+      <Button variant="secondary">
+        <UserCog className="size-5" />
+        Gerenciar convidados
+      </Button>
+    </div>
+  )
+}
