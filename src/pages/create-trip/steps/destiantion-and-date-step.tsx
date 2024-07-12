3a697586032fafffa242c6b481a7cf01ee23517e
Hudson Holanda <hudsonholanda55@gmail.com>
2024-07-12 20:01:31 -0300

    refact: migrate to button component

--- src/pages/create-trip/steps/destiantion-and-date-step.tsx
+++ src/pages/create-trip/steps/destiantion-and-date-step.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { Button } from "../../../components/button";
 
 interface DestinationAndDateStepProps {
   isGuestsInputOpen: boolean
@@ -21,13 +22,13 @@ export function DestinationAndDateStep({ isGuestsInputOpen, closeGuestsInput, op
       <div className="w-px h-6 bg-zinc-800"></div>
 
       {isGuestsInputOpen ? (
-        <button onClick={closeGuestsInput} className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+        <Button variant="secondary" onClick={closeGuestsInput}>
           Alterar local/data <Settings2 className="size-5" />
-        </button>
+        </Button>
       ) : (
-        <button onClick={openGuestsInput} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
-          Continuar<ArrowRight className="size-5" />
-        </button>
+        <Button onClick={openGuestsInput}>
+          Continuar <ArrowRight className="size-5" />
+        </Button>
       )}
     </div>
 
