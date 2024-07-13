99ea4d72bf3532ab9d9f32780aac85bde8631c55
Hudson Holanda <hudsonholanda55@gmail.com>
2024-07-13 17:49:41 -0300

    feat: get important links from api

--- src/pages/trip-details/important-links.tsx
+++ src/pages/trip-details/important-links.tsx
@@ -1,10 +1,21 @@
 import { Link2, Plus } from "lucide-react";
 import { Button } from "../../components/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AddImportantLinkModal } from "./add-important-link-modal";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+
+interface Link {
+  id: string
+  title: string
+  trip_id: string
+  url: string
+}
 
 export function ImportantLinks() {
   const [showAddImportantLinkModal, setShowAddImportantLinkModal] = useState(false);
+  const [importantLinks, setImportantLinks] = useState<Link[]>([]);
+  const { tripId } = useParams();
 
   function openShowAddImportantLinkModal() {
     setShowAddImportantLinkModal(true)
@@ -14,27 +25,31 @@ export function ImportantLinks() {
     setShowAddImportantLinkModal(false)
   }
 
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}/links`).then(response => {
+      setImportantLinks(response.data.links);
+    })
+  }, [tripId])
+
   return (
     <>
       <div className="space-y-6">
         <h2 className="font-semibold text-xl">Link importantes</h2>
         <div className="space-y-5">
-          <div className="flex items-center justify-between">
-            <div className="space-y-5">
-              <span className="block font-medium  text-zinc-100">Reserva do AirBnB</span>
-              <span className="block text-xs truncate text-zinc-400 hover:text-zinc-200">
-                https://www.airbnb.com.br/rooms/611568560615551716?search_mode=regular_search&check_in=2024-07-20&check_out=2024-07-27&source_impression_id=p3_1720887167_P3VUgqslrZQXq3Wo&previous_page_section_name=1000&federated_search_id=bd80a8b8-3d37-451b-a403-d0401cf3932e            </span>
-            </div>
-            <Link2 className="text-zinc-400 size-5 shrink-0" />
-          </div>
-          <div className="flex items-center justify-between">
-            <div className="space-y-5">
-              <span className="block font-medium  text-zinc-100">Reserva do AirBnB</span>
-              <span className="block text-xs truncate text-zinc-400 hover:text-zinc-200">
-                https://www.airbnb.com.br/rooms/611568560615551716?search_mode=regular_search&check_in=2024-07-20&check_out=2024-07-27&source_impression_id=p3_1720887167_P3VUgqslrZQXq3Wo&previous_page_section_name=1000&federated_search_id=bd80a8b8-3d37-451b-a403-d0401cf3932e            </span>
-            </div>
-            <Link2 className="text-zinc-400 size-5 shrink-0" />
-          </div>
+          {importantLinks.map(link => {
+            return (
+              <div key={link.id} className="flex items-center justify-between">
+                <div className="space-y-5">
+                  <span className="block font-medium  text-zinc-100">{link.title}</span>
+                  <a target="_blank" href={link.url} className="block text-xs truncate text-zinc-400 hover:text-zinc-200">
+                    {link.url}
+                  </a>
+                </div>
+                <Link2 className="text-zinc-400 size-5 shrink-0" />
+              </div>
+            )
+          })}
         </div>
         <Button onClick={openShowAddImportantLinkModal} variant="secondary" >
           <Plus className="size-5" />
