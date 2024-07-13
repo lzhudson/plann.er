6978ba8ef3b58f7badb8e506a2b38fe5ddf7a094
Hudson Holanda <hudsonholanda55@gmail.com>
2024-07-12 21:38:45 -0300

    feat: show all guests

--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,29 +1,46 @@
-import { CircleDashed, UserCog } from "lucide-react";
+import { CheckCircle2, CircleDashed, UserCog } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+
+interface Participant {
+  id: string
+  name: string | null
+  email: string
+  is_confirmed: boolean
+}
 
 export function Guests() {
+  const { tripId } = useParams();
+  const [participants, setParticipants] = useState<Participant[]>([]);
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}/participants`).then(response => setParticipants(response.data.participants))
+  }, [tripId])
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Convidados</h2>
       <div className="space-y-5">
-        <div className="flex items-center justify-between">
-          <div className="space-y-5">
-            <span className="block font-medium  text-zinc-100">Jessia White</span>
-            <span className="block text-xs truncate text-zinc-400 hover:text-zinc-200">
-              jessicawhite44@yahoo.com.br
-            </span>
-          </div>
-          <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-        </div>
-        <div className="flex items-center justify-between">
-          <div className="space-y-5">
-            <span className="block font-medium  text-zinc-100">Dr. Rita Pacocha</span>
-            <span className="block text-xs truncate text-zinc-400 hover:text-zinc-200">
-              ritapacocha@yahoo.com.br
-            </span>
-          </div>
-          <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-        </div>
+        {participants.map((participant, index) => {
+          return (
+            <div key={participant.id} className="flex items-center justify-between">
+              <div className="space-y-5">
+                <span className="block font-medium  text-zinc-100">{participant.name ?? `Convidado ${index}`}</span>
+                <span className="block text-xs truncate text-zinc-400 hover:text-zinc-200">
+                  {participant.email}
+                </span>
+              </div>
+              {participant.is_confirmed ? (
+                <CheckCircle2 className="size-5 shrink-0 text-green-400" />
+              ) : (
+                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+              )}
+            </div>
+          )
+        })}
+
       </div>
       <Button variant="secondary">
         <UserCog className="size-5" />
