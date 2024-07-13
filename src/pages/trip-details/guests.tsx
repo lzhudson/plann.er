0d1a75027e6d62777c65b4b936495069e42f8dad
Hudson Holanda <hudsonholanda55@gmail.com>
2024-07-13 18:10:09 -0300

    feat: create state, functions and use AddNewGuestModal component

--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -3,6 +3,7 @@ import { Button } from "../../components/button";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { api } from "../../lib/axios";
+import { AddNewGuestModal } from "./add-new-guest-modal";
 
 interface Participant {
   id: string
@@ -14,38 +15,55 @@ interface Participant {
 export function Guests() {
   const { tripId } = useParams();
   const [participants, setParticipants] = useState<Participant[]>([]);
+  const [isAddNewGuestModalOpen, setIsAddNewGuestModalOpen] = useState(false);
+
+  function closeAddNewGuestModal() {
+    setIsAddNewGuestModalOpen(false);
+  }
+
+  function openAddNewGuestModal() {
+    setIsAddNewGuestModalOpen(true);
+  }
+
 
   useEffect(() => {
     api.get(`/trips/${tripId}/participants`).then(response => setParticipants(response.data.participants))
   }, [tripId])
 
   return (
-    <div className="space-y-6">
-      <h2 className="font-semibold text-xl">Convidados</h2>
-      <div className="space-y-5">
-        {participants.map((participant, index) => {
-          return (
-            <div key={participant.id} className="flex items-center justify-between">
-              <div className="space-y-5">
-                <span className="block font-medium  text-zinc-100">{participant.name ?? `Convidado ${index}`}</span>
-                <span className="block text-xs truncate text-zinc-400 hover:text-zinc-200">
-                  {participant.email}
-                </span>
+    <>
+      <div className="space-y-6">
+        <h2 className="font-semibold text-xl">Convidados</h2>
+        <div className="space-y-5">
+          {participants.map((participant, index) => {
+            return (
+              <div key={participant.id} className="flex items-center justify-between">
+                <div className="space-y-5">
+                  <span className="block font-medium  text-zinc-100">{participant.name ?? `Convidado ${index}`}</span>
+                  <span className="block text-xs truncate text-zinc-400 hover:text-zinc-200">
+                    {participant.email}
+                  </span>
+                </div>
+                {participant.is_confirmed ? (
+                  <CheckCircle2 className="size-5 shrink-0 text-green-400" />
+                ) : (
+                  <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+                )}
               </div>
-              {participant.is_confirmed ? (
-                <CheckCircle2 className="size-5 shrink-0 text-green-400" />
-              ) : (
-                <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-              )}
-            </div>
-          )
-        })}
+            )
+          })}
 
+        </div>
+        <Button onClick={openAddNewGuestModal} variant="secondary">
+          <UserCog className="size-5" />
+          Gerenciar convidados
+        </Button>
       </div>
-      <Button variant="secondary">
-        <UserCog className="size-5" />
-        Gerenciar convidados
-      </Button>
-    </div>
+      {isAddNewGuestModalOpen && (
+        <AddNewGuestModal 
+          closeAddImportantLinkModal={closeAddNewGuestModal}
+        />
+      )}
+    </>
   )
 }
