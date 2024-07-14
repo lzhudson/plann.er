39039251383c1c8dd549b298c93852a43b66b1f4
Hudson Holanda <hudsonholanda55@gmail.com>
2024-07-14 09:23:21 -0300

    fix: add margin and format occurs at hour

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -33,7 +33,7 @@ export function Activities() {
               <span className="text-xs text-zinc-500">{format(category.date, 'EEEE', { locale: ptBR })}</span>
             </div>
             {category.activities.length > 0 ? (
-              <div>
+              <div className="space-y-3">
                 {category.activities.map(activity => {
                   return (
                     <div key={activity.id} className="space-y-2.5">
@@ -41,7 +41,7 @@ export function Activities() {
                         <CircleCheck className="size-5 text-lime-300" />
                         <span className="text-zinc-100">{activity.title}</span>
                         <span className="text-zinc-400 text-sm ml-auto">
-                          {format(activity.occurs_at, 'HH:MM')}h
+                          {format(activity.occurs_at, 'HH:mm')}h
                         </span>
                       </div>
                     </div>
